e2c1e0921a5cbe6129fcdd832396d0c66f3649d5
unknown <quynhlt@qsq-PC.qsoft.com.vn>
2013-06-07 13:52:09 +0700

    clean

--- BankAccountDay03/src/main/java/bank/BankAccount.java
+++ BankAccountDay03/src/main/java/bank/BankAccount.java
@@ -11,12 +11,13 @@ import main.java.bank.entity.Account;
  */
 public class BankAccount {
 	private static final int INVALID_BALANCE = -1;
+	private static final int MAXLENGTH = 10;
 
 	public static int open(Account account) {
 		int balance = 0;
 		if (account.getAccountNumber().isEmpty()) {
 			balance = INVALID_BALANCE;
-		} else if (account.getAccountNumber().length() != 10) {
+		} else if (account.getAccountNumber().length() != MAXLENGTH) {
 			balance = INVALID_BALANCE;
 		}
 		return balance;
